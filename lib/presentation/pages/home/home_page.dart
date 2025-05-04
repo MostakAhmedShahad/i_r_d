d596686c19c15e9c11c5f91f2ba325d3d6634010
Mostak <mostakahmedshahad74@gmail.com>
2025-05-04 21:30:35 +0600

    home_page_responsive

--- lib/presentation/pages/home/home_page.dart
+++ lib/presentation/pages/home/home_page.dart
@@ -3,127 +3,128 @@ import 'package:get/get.dart';
 import 'package:i_r_d/presentation/controllers/home_controller.dart';
 import 'package:i_r_d/presentation/pages/home/widgets.dart';
 import 'package:i_r_d/presentation/pages/menu/menu_page.dart';
-import 'package:i_r_d/presentation/widgets/catagory_chip.dart';
 import 'package:i_r_d/presentation/widgets/property_card.dart';
+ 
 
 class HomePage extends StatelessWidget {
   final controller = Get.find<HomeController>();
 
   @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+    final padding = size.width * 0.04;
+
     return Scaffold(
-      drawer: MenuPage(),
+      drawer:   MenuPage(),
       body: Builder(
-        builder: (context) => Obx(() => SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 12),
+        builder: (context) => Obx(() => SafeArea(
+              child: SingleChildScrollView(
+                padding: EdgeInsets.symmetric(horizontal: padding),
                 child: Column(
                   crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
-                    Padding(
-                      padding: const EdgeInsets.symmetric(
-                          horizontal: 12.0, vertical: 8.0),
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            crossAxisAlignment: CrossAxisAlignment.start,
-                            children: [
-                              Text("Location",
-                                  style: TextStyle(
-                                      color: Colors.grey, fontSize: 12)),
-                              Row(
-                                children: [
-                                  Text("Jakarta",
-                                      style: TextStyle(
-                                          fontSize: 16,
-                                          fontWeight: FontWeight.bold,
-                                          color: Colors.black)),
-                                  Icon(Icons.keyboard_arrow_down,
-                                      color: Colors.black),
-                                ],
-                              ),
-                            ],
-                          ),
+                    SizedBox(height: size.height * 0.02),
 
-                          // Notification
-                          Stack(
-                            children: [
-                              IconButton(
-                                icon: Icon(Icons.notifications_none,
-                                    color: Colors.black),
-                                onPressed: () {
-                                  Scaffold.of(context).openDrawer();
-                                },
-                              ),
-                              Positioned(
-                                right: 8,
-                                top: 8,
-                                child: Container(
-                                  width: 10,
-                                  height: 10,
-                                  decoration: BoxDecoration(
-                                    color: Colors.red,
-                                    shape: BoxShape.circle,
-                                  ),
+                    // Top bar
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text("Location",
+                                style: TextStyle(
+                                    color: Colors.grey,
+                                    fontSize: size.width * 0.03)),
+                            Row(
+                              children: [
+                                Text("Jakarta",
+                                    style: TextStyle(
+                                        fontSize: size.width * 0.045,
+                                        fontWeight: FontWeight.bold)),
+                                const Icon(Icons.keyboard_arrow_down),
+                              ],
+                            ),
+                          ],
+                        ),
+                        Stack(
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.notifications_none),
+                              onPressed: () {
+                                Scaffold.of(context).openDrawer();
+                              },
+                            ),
+                            Positioned(
+                              right: 8,
+                              top: 8,
+                              child: Container(
+                                width: 10,
+                                height: 10,
+                                decoration: const BoxDecoration(
+                                  color: Colors.red,
+                                  shape: BoxShape.circle,
                                 ),
-                              )
-                            ],
-                          )
-                        ],
-                      ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
                     ),
+
+                    SizedBox(height: size.height * 0.015),
 
                     // Search bar
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: Row(
-                        children: [
-                          Expanded(
-                            child: TextField(
-                              decoration: InputDecoration(
-                                hintText: 'Search address, or near you',
-                                prefixIcon: Icon(Icons.search),
-                                border: OutlineInputBorder(
-                                  borderRadius: BorderRadius.circular(12),
-                                  borderSide: BorderSide.none,
-                                ),
-                                filled: true,
-                                fillColor: Colors.grey[200],
+                    Row(
+                      children: [
+                        Expanded(
+                          child: TextField(
+                            decoration: InputDecoration(
+                              hintText: 'Search address, or near you',
+                              prefixIcon: const Icon(Icons.search),
+                              contentPadding: const EdgeInsets.symmetric(
+                                  vertical: 0, horizontal: 16),
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(12),
+                                borderSide: BorderSide.none,
                               ),
+                              filled: true,
+                              fillColor: Colors.grey[200],
                             ),
                           ),
-                          SizedBox(width: 10),
-                          Container(
-                            decoration: BoxDecoration(
-                              color: Colors.blueAccent,
-                              borderRadius: BorderRadius.circular(12),
-                            ),
-                            child: IconButton(
-                              icon: Icon(Icons.tune, color: Colors.white),
-                              onPressed: () {},
-                            ),
+                        ),
+                        SizedBox(width: size.width * 0.02),
+                        Container(
+                          decoration: BoxDecoration(
+                            color: Colors.blueAccent,
+                            borderRadius: BorderRadius.circular(12),
                           ),
-                        ],
-                      ),
+                          child: IconButton(
+                            icon: const Icon(Icons.tune, color: Colors.white),
+                            onPressed: () {},
+                          ),
+                        ),
+                      ],
                     ),
 
-                    SizedBox(height: 16),
+                    SizedBox(height: size.height * 0.02),
 
-                    // Category chips
+                    // Category Chips
                     SingleChildScrollView(
                       scrollDirection: Axis.horizontal,
                       child: Row(
                         children: ['House', 'Apartment', 'Hotel room', 'Villa']
-                            .map((e) {
+                            .map((category) {
                           final isSelected =
-                              controller.selectedCategory.value == e;
+                              controller.selectedCategory.value == category;
                           return GestureDetector(
-                            onTap: () => controller.changeCategory(e),
+                            onTap: () => controller.changeCategory(category),
                             child: Container(
-                              margin: EdgeInsets.only(right: 8),
+                              margin:
+                                  EdgeInsets.only(right: size.width * 0.02),
                               padding: EdgeInsets.symmetric(
-                                  horizontal: 16, vertical: 8),
+                                  horizontal: size.width * 0.04,
+                                  vertical: size.height * 0.01),
                               decoration: BoxDecoration(
                                 color: isSelected
                                     ? Colors.blueAccent
@@ -136,13 +137,15 @@ class HomePage extends StatelessWidget {
                                 ),
                               ),
                               child: Text(
-                                e,
+                                category,
                                 style: TextStyle(
-                                  color:
-                                      isSelected ? Colors.white : Colors.black,
+                                  color: isSelected
+                                      ? Colors.white
+                                      : Colors.black,
                                   fontWeight: isSelected
                                       ? FontWeight.bold
                                       : FontWeight.normal,
+                                  fontSize: size.width * 0.035,
                                 ),
                               ),
                             ),
@@ -151,49 +154,49 @@ class HomePage extends StatelessWidget {
                       ),
                     ),
 
-                    SizedBox(height: 20),
+                    SizedBox(height: size.height * 0.03),
 
                     // Near from you
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Near from you",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text("See more", style: TextStyle(color: Colors.blue)),
-                      ],
-                    ),
-
-                    SizedBox(height: 10),
+                    _sectionHeader(context, "Near from you"),
+                    SizedBox(height: size.height * 0.01),
                     PropertyCardList(properties: controller.properties),
 
-                    SizedBox(height: 20),
+                    SizedBox(height: size.height * 0.03),
 
                     // Best for you
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Text("Best for you",
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold)),
-                        Text("See more", style: TextStyle(color: Colors.blue)),
-                      ],
-                    ),
-
-                    SizedBox(height: 10),
-
-                    // Vertical property tiles
+                    _sectionHeader(context, "Best for you"),
+                    SizedBox(height: size.height * 0.01),
                     Column(
                       children: controller.properties
-                          .map((property) => PropertyTile(property: property))
+                          .map((p) => Padding(
+                                padding: EdgeInsets.only(
+                                    bottom: size.height * 0.015),
+                                child: PropertyTile(property: p),
+                              ))
                           .toList(),
                     ),
 
-                    SizedBox(height: 20),
+                    SizedBox(height: size.height * 0.04),
                   ],
                 ),
               ),
             )),
+      ),
+    );
+  }
+
+  Widget _sectionHeader(BuildContext context, String title) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 4),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: [
+          Text(title,
+              style: TextStyle(
+                  fontSize: MediaQuery.of(context).size.width * 0.045,
+                  fontWeight: FontWeight.bold)),
+          const Text("See more", style: TextStyle(color: Colors.blue)),
+        ],
       ),
     );
   }
